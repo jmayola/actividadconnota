42d0cd119dd3992ae8711aa7c5e5ff350453e2e6
mayola <mayola@marketpsychdata.com>
2024-05-16 15:25:32 -0300

    final hora, agregado cambio de color en colision, may god spare us all

--- actividadconnota.js
+++ actividadconnota.js
@@ -1,13 +1,13 @@
 // posiciones
-let ellX1 = 350
+let ellX2 = 50
+let ellY2 = 30
+let ellX1 = 100
 let ellY1 = 100
-let ellX2 = 200
-let ellY2 = 100
 //velocidades
-let movX1 = 2
-let movY1 = 2
-let movX2 = 5
-let movY2 = 5
+let movX1 = 6
+let movY1 = 6
+let movX2 = 4
+let movY2 = 4
 //colores
 let r1 = 255
 let g1 = 0
@@ -32,34 +32,61 @@ function draw() {
   ellY1 = ellY1 + movY1
   ellX2 = ellX2 + movX2
   ellY2 = ellY2 + movY2
-  
+  fill(0,0,0)  
   text(ellX1, 100,100) 
   text(ellY1, 200,200) 
   text(ellX2, 300,300) 
   text(ellY2, 400,400) 
    
-  if(ellX1 > 575 || ellX1 < 25){
+  if(ellX1 >= 575 || ellX1 <= 25){
     movX1 = movX1 * (-1)
   }
-  
-  if(ellX2 > 575 || ellX2 < 25){
+  if(ellX2 >= 575 || ellX2 <= 25){
     movX2 = movX2 * (-1)
-    text("true",200,300)
   }
-  if(ellY1 > 575 || ellY1 < 25){
+  if(ellY1 >= 575){
     movY1 = movY1 * (-1)
+        ellY1 = ellY1 - 4
+
   }
-  if(ellY2 > 575 || ellY2 < 25){
+  if(ellY1 <= 25){
+    movY1 = movY1 * (-1)
+    r1 = int(random(0,255))
+        ellY1 = ellY1 + 4
+  }
+  if(ellY2 >= 575){ //colicion azul tocando el piso
     movY2 = movY2 * (-1)
+        b2 = int(random(0,255))
+        ellY2 = ellY2 - 4
+  }
+  if(ellY2 <= 25){
+    movY2 = movY2 * (-1)
+        ellY2 = ellY2 + 4
   }
   h2 =((ellY1 - ellY2)* (ellY1 - ellY2)) + ((ellX1 - ellX2) * (ellX1 - ellX2))
   distancia =sqrt(h2);
   text(distancia, 350,500)
-  if(distancia < 50){
+  if(distancia <= 50){
+    changeColor()
     text("colicion",0,500)
-        movX1 = movX1 * (-1)
-        movY2 = movY1 * (-1)
-        movX2 = movX2 * (-1)
-        movY2 = movY2 * (-1)
+    ellX1 = ellX1 - 4
+    ellX2 = ellX2 + 4
+    ellY1 = ellY1 - 4
+    ellY2 = ellY2 + 4
+
+    movX1 = movX1 * (-1)
+    movY2 = movY1 * (-1)
+    movX2 = movX2 * (-1)
+    movY2 = movY2 * (-1)
+    r2 = r2 - (255 / 90)
+    b1 = b1 - (255 / 90)
+    
   }
+}
+function changeColor(){
+      r2 = 255
+    b1 = 255
+    r1 = 255
+    b2 = 255
+
 }
